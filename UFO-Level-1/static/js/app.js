1d203a03f07bd9c7e2e274d8549f5c649a78f100
Patrick Spitzer <rick.spitzer@yahoo.com>
2021-03-05 21:15:13 -0700

    testing filter

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -1,4 +1,34 @@
 // from data.js
 var tableData = data;
 
+var tbody = d3.select("tbody");
+
 // YOUR CODE HERE!
+
+data.forEach((ufoSeen) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoSeen).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+  
+  var button = d3.select("#filter-btn");
+
+  var inputField = d3.select("#form");
+
+  button.on("click", runEnter);
+  form.on("submit", runEnter);
+
+  function runEnter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+  
+    // Select the input element and get the raw HTML node
+    var inputForm = d3.select("#datetime");
+  
+    // Get the value property of the input element
+    var inputValue = inputForm.property("value");
+  }
+  
