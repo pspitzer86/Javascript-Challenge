9d3481b5c929178f03c13c7988e379f760174df5
Patrick Spitzer <rick.spitzer@yahoo.com>
2021-03-07 17:27:15 -0700

    adjusting to filter by other columns

--- UFO-Level-2/static/js/app.js
+++ UFO-Level-2/static/js/app.js
@@ -15,27 +15,57 @@ data.forEach((ufoSeen) => {
   
   var button = d3.select("#filter-btn");
 
-  var form = d3.select("#form");
-
   button.on("click", runEnter);
-  form.on("submit", runEnter);
 
   function runEnter() {
 
     // Prevent the page from refreshing
     d3.event.preventDefault();
   
-    // Select the input element and get the raw HTML node
-    var inputForm = d3.select("#datetime");
-  
-    // Get the value property of the input element
-    var inputValue = inputForm.property("value");
+    // Select the input element and get the raw HTML node, and get the value property of the input element
+    var inputDate = d3.select("#datetime");
+    var inputDvalue = inputDate.property("value");
+
+    if (inputDvalue === "") {
+      inputDvalue = "/*/"
+    }
+
+    // Select the input element and get the raw HTML node, and get the value property of the input element
+    var inputCity = d3.select("#city");
+    var inputCvalue = inputCity.property("value");
+
+    if (inputCvalue === "") {
+      inputCvalue = "/*/"
+    }   
+
+    // Select the input element and get the raw HTML node, and get the value property of the input element
+    var inputState = d3.select("#state");
+    var inputSvalue = inputState.property("value");
+
+    if (inputSvalue === "") {
+      inputSvalue = "/*/"
+    }        
+
+    // Select the input element and get the raw HTML node, and get the value property of the input element
+    var inputCountry = d3.select("#country");
+    var inputCOvalue = inputCountry.property("value");
+
+    if (inputCOvalue === "") {
+      inputCOvalue = "/*/"
+    }
+    // Select the input element and get the raw HTML node, and get the value property of the input element
+    var inputShape = d3.select("#shape");
+    var inputSvalue = inputShape.property("value");
+
+    if (inputSvalue === "") {
+      inputSvalue = "/*/"
+    }    
 
     if (inputValue === "") {
         var filteredData = tableData;
     }
     else {
-        var filteredData = tableData.filter(ufo => ufo.datetime === inputValue);
+        var filteredData = tableData.filter(ufo => ufo.datetime === inputDvalue);
     }
 
     tbody.html("");
