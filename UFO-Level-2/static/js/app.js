dcc30a2b60d48f0a716f3dc380a354cc31c947aa
Patrick Spitzer <rick.spitzer@yahoo.com>
2021-03-07 21:44:20 -0700

    center no sightings found text using css

--- UFO-Level-2/static/js/app.js
+++ UFO-Level-2/static/js/app.js
@@ -45,6 +45,7 @@ data.forEach((ufoSeen) => {
         var td = d3.select("td");
 
         td.attr("colspan", "7");
+        td.attr("class", "none-found")
         cell.text(`No sightings reported`);
     }
     else {
