88e8322b91fd8605d223a6e795345a47edb7ed25
Patrick Spitzer <rick.spitzer@yahoo.com>
2021-03-07 22:18:23 -0700

    commented

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -3,7 +3,7 @@ var tableData = data;
 
 var tbody = d3.select("tbody");
 
-// YOUR CODE HERE!
+// Running through each object in the data.js and appending all values to tr and td tags to make a table for the html.
 
 data.forEach((ufoSeen) => {
     var row = tbody.append("tr");
@@ -12,6 +12,8 @@ data.forEach((ufoSeen) => {
       cell.text(value);
     });
   });
+
+  // Creating a variable for the button and input form that have events set to them to run a function.
   
   var button = d3.select("#filter-btn");
 
@@ -19,6 +21,8 @@ data.forEach((ufoSeen) => {
 
   button.on("click", runEnter);
   form.on("submit", runEnter);
+
+  // Function called on event to compare the value inputed to values in the data table in order to filter them by date.
 
   function runEnter() {
 
@@ -31,12 +35,18 @@ data.forEach((ufoSeen) => {
     // Get the value property of the input element
     var inputValue = inputForm.property("value");
 
+    // If there is no input, defaults to the entire data table, else filter the data by the inputed datetime.
+
     if (inputValue === "") {
         var filteredData = tableData;
     }
     else {
         var filteredData = tableData.filter(ufo => ufo.datetime === inputValue);
     }
+
+    // First clear the table body of the html to create new table on filtered data.  If the input does not match anything in the table and therefore
+    // has a length of 0 after filtering, make a message taking up all columns claiming there are no sightings for that date.  Else, create new
+    // tr and td tags to create a table out of the values from the filtered data.
 
     tbody.html("");
 
@@ -57,6 +67,8 @@ data.forEach((ufoSeen) => {
               cell.text(value);
             });
           });
+
+    // After the event occurs, clear the input.
         
     inputForm.property("value", "");
 
--- UFO-Level-2/static/js/app.js
+++ UFO-Level-2/static/js/app.js
@@ -3,7 +3,7 @@ var tableData = data;
 
 var tbody = d3.select("tbody");
 
-// YOUR CODE HERE!
+// Running through each object in the data.js and appending all values to tr and td tags to make a table for the html.
 
 data.forEach((ufoSeen) => {
     var row = tbody.append("tr");
@@ -12,19 +12,32 @@ data.forEach((ufoSeen) => {
       cell.text(value);
     });
   });
+
+  // Creating a variable for the button that has an event set to it to run a function.
   
   var button = d3.select("#filter-btn");
 
   button.on("click", runEnter);
 
+  // Function called on event to compare the value inputed to values in the data table in order to filter them by date, city, state, country and shape.
+
   function runEnter() {
 
     // Prevent the page from refreshing
+
     d3.event.preventDefault();
+
+    // Setting the page to the whole data table if there is no input.
   
     var filterData = tableData;
 
+    // Setting a list of the five columns to be potentially filtered on.
+
     var colList = ["datetime", "city", "state", "country", "shape"];
+
+    // For loop that runs through the column list that calls the id for each column from the html and grabs the values to be compared with
+    // whatever information is inputted in any of the five filters.  Setting the input and the table data to lowercase to compare them if the input
+    // contains any capital letters.  Filters out any data that matches any of the inputs then clears the inputs.
 
     for (var i = 0; i < colList.length; i++) {
       var inputCol = d3.select("#" + colList[i]);
@@ -36,6 +49,10 @@ data.forEach((ufoSeen) => {
         inputCol.property("value", "");
       }
     }
+
+    // First clear the table body of the html to create new table on filtered data.  If the input does not match anything in the table and therefore
+    // has a length of 0 after filtering, make a message taking up all columns claiming there are no sightings for the inputs.  Else, create new
+    // tr and td tags to create a table out of the values from the filtered data.
 
     tbody.html("");
 
